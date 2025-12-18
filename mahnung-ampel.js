af760fe6d074ccce79b27d2254aef4b4bccfd6bb
BLameui33 <alexanderdemirel@gmail.com>
2025-12-18 14:41:51 +0000

    cc

--- mahnung-ampel.js
+++ mahnung-ampel.js
@@ -167,7 +167,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
                     const opt = {
                         margin:       [10, 10], // Ränder in mm (besser als Inch)
-                        filename:     'ratenplan-uebersicht.pdf',
+                        filename:     'mahnung-uebersicht.pdf',
                         image:        { type: 'jpeg', quality: 0.98 },
                         html2canvas:  { scale: 2, useCORS: true, logging: false }, // Scale 2 sorgt für scharfe Schrift
                         jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
